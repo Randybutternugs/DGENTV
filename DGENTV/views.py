337d436970902c5a896a535e35380a7907a97209
Randybutternugs <adonisalexis@icloud.com>
2024-12-29 05:44:17 -0500

    basic setup stuff

--- DGENTV/views.py
+++ DGENTV/views.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+import requests
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("homepage.html")
+
+@views.route('/stats', methods=['GET'])
+def get_affiliate_stats():
+    # ... your route logic here ...
+    return "Some response"
